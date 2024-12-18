5cfba88570c9c10bcad6c84fc2f596d0e2171ab4
rpatel1992 <rpatel@qeleo.net>
2024-12-18 18:54:05 +0530

    Update ZAPSecurityTest.java

--- ZAPSecurityTest.java
+++ ZAPSecurityTest.java
@@ -1,79 +1,39 @@
-package Test;
-
-import org.testng.annotations.Test;
-import org.openqa.selenium.Proxy;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.CapabilityType;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.zaproxy.clientapi.core.ApiResponse;
 import org.zaproxy.clientapi.core.ClientApi;
 import org.zaproxy.clientapi.core.ClientApiException;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+public class ZapScan {
+    private static final String ZAP_ADDRESS = "localhost";
+    private static final int ZAP_PORT = 8080;
+    private static final String ZAP_API_KEY = "irmje6k7mttb7dlqmvfvoeqvij";
 
-public class ZAPSecurityTest {
+    public static void main(String[] args) {
+        ClientApi api = new ClientApi(ZAP_ADDRESS, ZAP_PORT, ZAP_API_KEY);
+        String targetUrl = ""https://www.mrtesting.com/";
 
-	static final String ZAP_PROXY_ADDRESS = "localhost";
-	static final int ZAP_PROXY_PORT = 8080;
-	static final String ZAP_API_KEY = "irmje6k7mttb7dlqmvfvoeqvij";
+        try {
+            // Start the passive scan
+            System.out.println("Accessing target: " + targetUrl);
+            api.accessUrl(targetUrl);
 
-	private WebDriver driver;
-	private ClientApi api;
+            // Start an active scan
+            System.out.println("Starting active scan...");
+            String scanId = api.ascan.scan(targetUrl, "true", "false", null, null, null);
 
-	@BeforeMethod
-	public void setup() {
-		String proxyServerURL = ZAP_PROXY_ADDRESS + ":" + ZAP_PROXY_PORT;
+            // Monitor the scan progress
+            int progress;
+            do {
+                Thread.sleep(5000);
+                progress = Integer.parseInt(api.ascan.status(scanId));
+                System.out.println("Scan progress: " + progress + "%");
+            } while (progress < 100);
 
-		Proxy proxy1 = new Proxy();
-		proxy1.setHttpProxy(proxyServerURL);
-		proxy1.setSslProxy(proxyServerURL);
-		
-		 DesiredCapabilities capabilities = DesiredCapabilities.chrome();
-	        capabilities.setCapability(CapabilityType.PROXY, proxy1);
-	        //capabilities.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
-	        capabilities.setCapability("acceptInsecureCerts", true);
-	        
-		//ChromeOptions co = new ChromeOptions();
-		//co.setProxy(proxy1);
-		WebDriverManager.chromedriver().setup();
-       
-		driver = new ChromeDriver(capabilities);
+            System.out.println("Scan complete!");
 
-		api = new ClientApi(ZAP_PROXY_ADDRESS, ZAP_PROXY_PORT, ZAP_API_KEY);
-	}
-
-	@Test(priority = 0)
-	public void amazonSecurityTest() {
-		 //Thread.sleep(5000);
-		driver.get("https://www.mrtesting.com/");
-		//Assert.assertTrue(driver.getTitle().contains("Amazon"));
-	}
-
-	@AfterMethod
-	public void tearDown() {
-		if (api != null) {
-			String title = "ZAP Security Report";
-			String template = "traditional-html";
-			String description = "This is zap security test report";
-			String reportfilename = "zap-report.html";
-			String targetFolder = System.getProperty("user.dir");
-
-			try {
-				ApiResponse response = api.reports.generate(title, template, null, description, null, null, null, null,
-						null, reportfilename, null, targetFolder, null);
-				System.out.println("ZAP report generated at this location:" + response.toString());
-			} catch (ClientApiException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
-		}
-		driver.quit();
-	}
-	// driver.quit();
+            // Retrieve and print the results
+            String report = new String(api.core.htmlreport());
+            System.out.println(report);
+        } catch (ClientApiException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
