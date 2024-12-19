173fc8a2a1cd2f810a8ae092574cbb7a46fe71f7
rpatel1992 <rpatel@qeleo.net>
2024-12-19 19:08:45 +0530

    Delete ClipixAutomation.bat

--- ClipixAutomation.bat
+++
@@ -1,5 +0,0 @@
-set projectLocation=D:\Rakesh\Selenium\Regi
-cd %projectLocation%
-set classpath=%projectLocation%\target\test-classes;%projectLocation%\lib\*;
-java org.testng.TestNG %projectLocation%\testng.xml
-pause
